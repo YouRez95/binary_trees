3bdfbec18437e954a1aa177146a4cb283242ad9d
Youness Elalouani <younesselalouani1995@gmail.com>
2023-11-30 23:01:45 +0100

    traverse postorder

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - traverse the tree postorder
+ *
+ * @tree: pointer to tree
+ * @func: pointer to func print
+ *
+ * Return: Nothing
+ */
+
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
