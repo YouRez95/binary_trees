4c69f89f4579981d9c117d0bc3b906ebd472a077
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-01 13:00:15 +0100

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,23 +1,23 @@
 #include "binary_trees.h"
 
-
 /**
- * binary_tree_h - calcul the height of a binary tree
+ * binary_tree_height - calcul the height of subtree
  *
- * @tree: root
+ * @tree : root
  *
- * Return: height
+ * Return: Height
  */
 
-
-
-int binary_tree_h(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t leftHeight, rightHeight;
+
 	if (tree == NULL)
 		return (0);
 
-	int leftHeight = binary_tree_h(tree->left);
-	int rightHeight = binary_tree_h(tree->right);
+	leftHeight = binary_tree_height(tree->left);
+
+	rightHeight = binary_tree_height(tree->right);
 
 	return (leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1);
 }
@@ -37,8 +37,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	int leftHeight = binary_tree_h(tree->left);
-	int rightHeight = binary_tree_h(tree->right);
+	int leftHeight = binary_tree_height(tree->left);
+	int rightHeight = binary_tree_height(tree->right);
 
 	return (leftHeight - rightHeight);
 }
